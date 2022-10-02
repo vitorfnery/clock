6a9c03ad0c9fa3e390a0d6361fefcd68ea827713
Vítor F. Nery <vitorfelipenery@gmail.com>
2022-10-02 15:37:12 -0300

    arrow rotation js updated

--- index.js
+++ index.js
@@ -14,10 +14,11 @@ const updateClock = function () {
   const second = currDate.getSeconds();
   const hourDeg = (hour / 12) * 360;
   hourEl.style.transform = `rotate(${hourDeg}deg)`;
-  const minuteDeg = (minute / 12) * 360;
+  const minuteDeg = (minute / 60) * 360;
   minuteEl.style.transform = `rotate(${minuteDeg}deg)`;
-  const secondDeg = (second / 12) * 360;
+  const secondDeg = (second / 60) * 360;
   secondEl.style.transform = `rotate(${secondDeg}deg)`;
+  console.log(hour, minute, second);
 };
 
 // updateClock();
